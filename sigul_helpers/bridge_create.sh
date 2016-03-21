60e94af6fc363d0e680ef7a9c971316a7f830534
James Tanner <tanner.jc@gmail.com>
2016-03-21 16:49:15 -0400

    update create scripts

--- sigul_helpers/bridge_create.sh
+++ sigul_helpers/bridge_create.sh
@@ -9,6 +9,7 @@ chown sigul:sigul $bridge_dir/*.db
 
 # https://access.redhat.com/documentation/en-US/Red_Hat_Directory_Server/9.0/html/Administration_Guide/Managing_SSL-Using_certutil.html
 certutil -d $bridge_dir -L -n my-ca -a > $bridge_dir/cacert.asc
+pk12util -d $bridge_dir -o $bridge_dir/ca.p12 -n my-ca
 
 certutil -K -d $bridge_dir
 certutil -L -d $bridge_dir
--- sigul_helpers/client_create.sh
+++ sigul_helpers/client_create.sh
@@ -5,6 +5,7 @@ rm -rf $client_dir ; mkdir -p $client_dir
 
 bridge_host=bridge.example.org
 bridge_dir=~/bridge
+mkdir -p $bridge_dir
 scp -r root@$bridge_host:/var/lib/sigul/* $bridge_dir/.
 
 certutil -d $client_dir -N
@@ -18,4 +19,13 @@ certutil -d $client_dir -S -n sigul-client-cert -s "CN=$(hostname -f)" -c my-ca 
 
 sed -i.bak 's/bridge-hostname.*/bridge-hostname: bridge.example.org/' /etc/sigul/client.conf
 sed -i.bak 's/server-hostname.*/server-hostname: server.example.org/' /etc/sigul/client.conf
-sed -i.bak 's/; user-name: nobody/user-name: admin/' /etc/sigul/server.conf
+sed -i.bak 's/; user-name: nobody/user-name: admin/' /etc/sigul/client.conf
+
+
+# opcheck
+sigul -v -v list-users
+
+# create the vagrant user 
+sigul new-user --admin --with-password vagrant
+
+
